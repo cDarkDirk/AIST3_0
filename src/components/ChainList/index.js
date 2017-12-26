e7029d7206c023366fcc282733ecc1c03aa57508
Orumant <30774551+Orumant@users.noreply.github.com>
2017-12-26 15:16:25 +0300

    FIX

--- src/components/ChainList/index.js
+++ src/components/ChainList/index.js
@@ -2,20 +2,27 @@ import React from "react"
 import {ListGroup, ListGroupItem, Badge} from 'react-bootstrap'
 
 import './style.css'
-const ChainList = ({chainTemplates, selectedChainTemplate, selectChainTemplate, dirtyChainTemplateIndicies}) => {
-    return (<ListGroup>
-        {
-            chainTemplates.map((chain, idx) => {
-                return <ListGroupItem
-                          key={idx}
-                          className='chain-list-item'
-                          href="#" active={idx===selectedChainTemplate}
-                          onClick={() => selectChainTemplate(idx)}>
-                          {chain.name}
-                          {dirtyChainTemplateIndicies[idx] && <Badge pullRight={true} bsStyle='success'>modified</Badge>}
-                          </ListGroupItem>
-            })
-        }
+
+const ChainList = ({
+                     chainTemplates,
+                     selectedChainTemplate,
+                     chainSelected,
+                     dirtyChainTemplateIndicies
+                   }) => {
+  return (
+    <ListGroup>
+      {
+        chainTemplates.map((chain, idx) => {
+          return <ListGroupItem
+            key={idx}
+            className='chain-list-item'
+            href="#" active={idx === selectedChainTemplate}
+            onClick={() => chainSelected(idx)}>
+            {chain.name}
+            {dirtyChainTemplateIndicies[idx] && <Badge pullRight={true} bsStyle='success'>modified</Badge>}
+          </ListGroupItem>
+        })
+      }
     </ListGroup>);
 
 }
