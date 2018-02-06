a92f02fb86dca718a9f8f1022be18d09a939cbc2
Dmitrii Leontev <d@sberbank.ru>
2018-02-06 17:45:45 +0300

    Merge branch 'master' of https://github.com/cDarkDirk/AIST3_0_FRONT into featers/LauncherPage
    # Conflicts:
    #	src/actions.js
    #	src/components/Form/index.js
    #	src/components/ScheduleForm/index.js
    #	src/components/TemplateForm/index.js
    #	src/containers/TemplateForm.js
    #	src/reducers/index.js

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,88 +1,81 @@
 import React from 'react'
-import {Jumbotron} from 'react-bootstrap'
-import {Row, Col, Grid, FormControl, FormGroup, ControlLabel, Form} from "react-bootstrap"
+import {Jumbotron, InputGroup} from 'react-bootstrap'
+import {Row, Col, Grid, FormControl, FormGroup, ControlLabel, Form, Checkbox, Button, Dropdown} from "react-bootstrap"
 import "./style.css"
+import Calendar from "react-calendar";
 import DatePicker from "react-datepicker"
 
 class MyForm extends React.Component {
 
-    componentDidMount() {
-        this.props.fetchFormTemplate(this.props.formName)
-    }
+  componentDidMount() {
+    this.props.fetchFormTemplate(this.props.formName)
+  }
 
-    render() {
-        const {formTemplate, formValues, onFormInputChange} = this.props;
+  render() {
+    const {formName, formTemplate, formValues, onFormInputChange} = this.props
 
-        const form = formTemplate ? (formTemplate.fields.map((field, index) => {
-            if (field.type === "text") {
-                return (
-                    <FormGroup controlId="formHorizontalInput">
-                        <Col componentClass={ControlLabel} sm={2}>
-                            {field.label}
-                        </Col>
-                        <Col sm={10}>
-                            <FormControl value={formValues[field.paramName]} type="input" placeholder="auto"
-                                         onChange={(event) => onFormInputChange(event.target.value, field.paramName, this.props.formName)}/>
-                        </Col>
-                    </FormGroup>
-                )
-            }
-            else if (field.type === "dropDown") {
-                return (
-                    <FormGroup controlId="formHorizontalDropDown">
-                        <Col componentClass={ControlLabel} sm={2}>
-                            {field.label}
-                        </Col>
-                        <Col sm={10}>
-                            <FormControl componentClass="select" value={formValues[field.paramName]}
-
-                                         type="input" placeholder="auto"
-                                         onChange={(event) => onFormInputChange(event.target.value, field.paramName, this.props.formName)}>
-                                {field.dropDownOptions.map((op, idx) => (<option key={idx} value={op}>{op}</option>))}
-                            </FormControl>
-                        </Col>
-                    </FormGroup>
-                )
-            }
-            else if (field.type === "calendar") {
-                return (
-                    <FormGroup controlId="calendar">
-                        <Col componentClass={ControlLabel} sm={2}>
-                            {field.label}
-                        </Col>
-
-                        <Col sm={10}>
-                            <div>
-                                <DatePicker key={index}
-                                            onChange={(date) => onFormInputChange(date, field.paramName, this.props.formName)}
-                                            selected={formValues[field.paramName]}
-                                />
-                            </div>
-                        </Col>
-                    </FormGroup>)
-            }
-            else return (<div>NO SUCH FIELD!</div>)
-        }).map((field, index) => {
-
-            return (<Col md={6}>{field}</Col>)
-        })) : (
-            <div> NO FORM TEMPLATE SPECIFIED</div>
-        );
-
+    const form = formTemplate ? (formTemplate.fields.map((field, index) => {
+      if (field.type === "text") {
         return (
-            <div className='container'>
-                <Jumbotron>
-                    <h1>Form</h1>
-                    <Grid>
-                        <Form horizontal>
-                            <Row>{form}</Row>
-                        </Form>
-                    </Grid>
-
-                </Jumbotron>
-            </div>
+          <FormGroup controlId="formHorizontalInput">
+            <InputGroup>
+              <InputGroup.Addon>{field.label}</InputGroup.Addon>
+              <FormControl value={formValues[field.paramName]} type="input" placeholder="auto"
+                           onChange={(event) => onFormInputChange(event.target.value, field.paramName, this.props.formName)}/>
+            </InputGroup>
+          </FormGroup>
         )
-    }
+      }
+      if (field.type === "dropDown") {
+        return (
+          <FormGroup controlId="formHorizontalDropDown">
+            <InputGroup>
+              <InputGroup.Addon>{field.label}</InputGroup.Addon>
+              <FormControl componentClass="select" value={formValues[field.paramName]}
+
+                           type="input" placeholder="auto"
+                           onChange={(event) => onFormInputChange(event.target.value, field.paramName, this.props.formName)}>
+                {field.dropDownOptions.map((op, idx) => (<option key={idx} value={op}>{op}</option>))}
+              </FormControl>
+            </InputGroup>
+          </FormGroup>
+
+        )
+      }
+      if (field.type === "calendar") {
+        return (
+          <FormGroup controlId="calendar">
+            <InputGroup>
+              <InputGroup.Addon>{field.label}</InputGroup.Addon>
+              <div   className={"form-date-picker"}>
+              <DatePicker key={index}
+                          onChange={(date) => onFormInputChange(date, field.paramName, this.props.formName)}
+                          selected={formValues[field.paramName]}
+              />
+              </div>
+            </InputGroup>
+          </FormGroup>)
+      }
+    }).map((field, index) => {
+
+      return (<Col md={6}>{field}</Col>)
+    })) : (
+      <div> NO FORM TEMPLATE SPECIFIED</div>
+    )
+
+    return (
+      <div className='container'>
+        <Jumbotron>
+          <Grid>
+            <Form horizontal>
+              <Row>{form}</Row>
+            </Form>
+          </Grid>
+
+        </Jumbotron>
+      </div>
+    )
+  }
 }
 
 export default MyForm
