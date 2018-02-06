1837d1fa2ced508b018f860e8783c5bac827e61b
SBT-Razlatskiy-SA <razlac94@gmail.com>
2018-02-06 15:26:25 +0400

    возвращаем моку первоначальный вид и chainsDisplay добавляем вывод консоли стейтов

--- src/containers/ChainDisplay.js
+++ src/containers/ChainDisplay.js
@@ -3,7 +3,9 @@ import {closeButtonClicked, testBlockMoved} from '../actions'
 import ChainDisplay from '../components/ChainDisplay'
 
 function mapStateToProps(state, ownProps) {
-    const test = state.test || [];
+
+  const test = state.test || [];
+  console.log(state);
   return {
     tests: ownProps.chainTemplate.tests.map(ct => test.find(test => test.test_id === ct.id))
   }
