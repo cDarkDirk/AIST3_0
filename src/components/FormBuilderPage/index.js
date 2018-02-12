87240725be41bffefcb0e745b78a35b490d9e0c4
Orumant <30774551+Orumant@users.noreply.github.com>
2018-02-12 13:25:18 +0300

    [+] formbuilder: post teporary fixed

--- src/components/FormBuilderPage/index.js
+++ src/components/FormBuilderPage/index.js
@@ -136,7 +136,7 @@ class FormBuilderPage extends React.Component {
 
   submitChanges = () => {
     const chainName = this.props.formBuilderChains[this.state.chainIndex].name;
-    const fields = this.props.formBuilderChains[this.state.chainIndex].fields;
+    const fields = this.props.formBuilderChains[this.state.chainIndex];
     const chainIndex = this.state.chainIndex;
     this.props.submit(chainName, fields, chainIndex);
   };
