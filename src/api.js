e5d89391ab399cf64f885b1009fb3325fd4ae0aa
Sergey <razlac94@gmail.com>
2018-02-15 10:54:01 +0400

    Comment encryption

--- src/api.js
+++ src/api.js
@@ -60,31 +60,31 @@ const fetchUtil = (url, method = 'GET', data = {}) => {
 
 
 export const updateLoginSucceed = (history) =>() => {
-  console.log("ЕЕЕ  РОК");
+  // console.log("ЕЕЕ  РОК");
   history.push('/homepage');
 };
 
-export const logFunction = (payload,history,publicKey) => (dispatch) => {
-  var a = JSON.stringify(publicKey);
-  var RSAKey = require('react-native-rsa');
-  var rsa = new RSAKey();
-  rsa.setPublicString(a);
-  payload.password = rsa.encrypt(payload.password);
-  dispatch(updateLoginForm(payload, history))
-}
-
-export const getPublicKey = (payload, history) =>(dispatch) =>{
-  if (payload.name === "" || payload.password === "") {
-    dispatch(error({message: "Error: Not all fields was filled"}));
-    return;
-  }
-  const url = `${BACKEND_URL}/login`;
-  axios.get(url).then(function (response) {
-    dispatch(logFunction(payload,history,response.data))
-  }).catch(function (response) {
-    dispatch(error({message: "Fetch failed with error!" + response}));
-  });
-};
+// export const logFunction = (payload,history,publicKey) => (dispatch) => {
+//   var a = JSON.stringify(publicKey);
+//   var RSAKey = require('react-native-rsa');
+//   var rsa = new RSAKey();
+//   rsa.setPublicString(a);
+//   payload.password = rsa.encrypt(payload.password);
+//   dispatch(updateLoginForm(payload, history))
+// }
+//
+// export const getPublicKey = (payload, history) =>(dispatch) =>{
+//   if (payload.name === "" || payload.password === "") {
+//     dispatch(error({message: "Error: Not all fields was filled"}));
+//     return;
+//   }
+//   const url = `${BACKEND_URL}/login`;
+//   axios.get(url).then(function (response) {
+//     dispatch(logFunction(payload,history,response.data))
+//   }).catch(function (response) {
+//     dispatch(error({message: "Fetch failed with error!" + response}));
+//   });
+// };
 export const putToken = (tokener) => (dispatch) => {
   // console.log(tokener);
 
@@ -92,7 +92,7 @@ export const putToken = (tokener) => (dispatch) => {
 }
 export const updateLoginForm = (payload, history) => (dispatch) => {
 
-  console.log("ЖАРА");
+  // console.log("ЖАРА");
 
   // console.log(payload.password);
   const url = `${BACKEND_URL}/login`;
--- src/containers/AuthorizationPage.js
+++ src/containers/AuthorizationPage.js
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux'
 import AuthorizationPage from '../components/AuthorizationPage'
 import {loginPasswordChange} from "../actions";
-import {getPublicKey} from "../api";
+import {updateLoginForm} from "../api";
 import  history from '../history.js'
 
 function mapStateToProps(state) {
@@ -13,7 +13,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    loginButtonClicked : (payload) => dispatch(getPublicKey(payload, history)),
+    loginButtonClicked : (payload) => dispatch(updateLoginForm(payload, history)),
     loginPasswordChange : (payload) => dispatch(loginPasswordChange(payload))
   }
 }
